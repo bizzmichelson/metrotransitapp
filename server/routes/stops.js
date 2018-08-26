eafbad95333721a4640e5d0bd499e2dc61e1b2e2
Bizz Michelson <bizz.michelson@gmail.com>
2018-08-25 22:26:12 -0500

    cleaned up

--- server/routes/stops.js
+++ server/routes/stops.js
@@ -14,33 +14,3 @@ myApp.controller('StopsController', function(dataService) {
   });
 });
 
-
-import React, { Component } from "react";
-import axios from "axios";
-import { Link } from "@reach/router";
-
-class Stops extends Component {
-  state = {
-    stops: []
-  };
-  async componentDidMount() {
-    const { direction, route } = this.props;
-    console.log(
-      `https://svc.metrotransit.org/NexTrip/Stops/${route}/${direction}`
-    );
-    const { data } = await axios(
-      `https://svc.metrotransit.org/NexTrip/Stops/${route}/${direction}`
-    );
-    this.setState({ stops: data });
-    console.log(data);
-  }
-  render() {
-    return this.state.stops.map(({ Text, Value }) => (
-      <div key={Value}>
-        <Link to={`/departures/${Value}`}>{Text}</Link>
-      </div>
-    ));
-  }
-}
-
-export default Stops;
